8e39cb40fe527b3849a7098b8ec9bbf5d7aba177
julietasegovia <julietavsegovia@gmail.com>
2026-05-03 14:47:26 -0300

    open/close logic + static text input implemented

--- notebook.js
+++ notebook.js
@@ -1,4 +1,27 @@
-const frontPage = document.querySelectorAll('.front-page');
-const actualNotebook = document.querySelectorAll('.actual-notebook');
+let frontPage = document.querySelector('.front-page');
+let actualNotebook = document.querySelector('.actual-notebook');
+let noteContent = document.querySelector('.note-content');
 
-actualNotebook.classList.add('.actual-notebook-closed');
+function openBook(){
+    actualNotebook.style.display = 'flex';
+    frontPage.style.display = 'none';
+}
+
+function closeBook(){
+    actualNotebook.style.display = 'none';
+    frontPage.style.display = 'flex';
+}
+ 
+actualNotebook.style.display = 'none';
+
+noteContent.addEventListener('keydown', function(e) {
+    if(e.key=='Enter'){
+        const lineHeight = parseFloat(getComputedStyle(this).lineHeight);
+        const maxLines = Math.floor(this.clientHeight / lineHeight);
+        const currentLines = this.innerText.split('\n').length;
+
+        if(currentLines >= maxLines){
+            e.preventDefault();
+        }
+    }
+});
