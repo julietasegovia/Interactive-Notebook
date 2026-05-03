ce9fd00ba1f723285c152e2a1057e7bd84426fd8
julietasegovia <julietavsegovia@gmail.com>
2026-05-03 12:07:30 -0300

    styling completed

---
+++ notebook.js
@@ -0,0 +1,4 @@
+const frontPage = document.querySelectorAll('.front-page');
+const actualNotebook = document.querySelectorAll('.actual-notebook');
+
+actualNotebook.classList.add('.actual-notebook-closed');
